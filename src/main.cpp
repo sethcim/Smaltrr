fb475119d6ef41a93742599f231239da40670375
sethcim <sethcim@mit.edu>
2020-12-31 17:18:56 -0500

    Disco lights and OLED for video

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,26 @@
 
+//Library includes
 #include <Arduino.h>
-#include <bitmaps.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
 #include <NeoPixelBus.h>
 #include <NeoPixelAnimator.h>
+
+//Additional font files
+#include <Fonts/FreeSans18pt7b.h>
+#include <Fonts/FreeSans24pt7b.h>
+
+//Project Source Includes
+#include <bitmaps.h>
 #include "disco.h"
 
 //Constants
-const uint8_t oledWidth = 128;
-const uint8_t oledHeight = 32;
+
 //const uint16_t PixelCount = 19; // make sure to set this to the number of pixels in your strip
 
 //Global manager objects
-Adafruit_SSD1306 display = Adafruit_SSD1306(oledWidth, oledHeight, &Wire);
+Adafruit_SSD1306 oled = Adafruit_SSD1306(128, 32, &Wire);
 
 //NeoPixelBus<DotStarBgrFeature, DotStarSpiMethod> strip(PixelCount);
 //NeoPixelAnimator animations(2); // only ever need 2 animations
@@ -40,39 +46,59 @@ void setup()
   //Serial messages over USB
   Serial.begin(112500);
 
-  //Display
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-
-  // Show image buffer on the display hardware.
-  // Since the buffer is intialized with an Adafruit splashscreen
-  // internally, this will display the splashscreen.
-  display.display();
-  delay(2000);
-  /*display.clearDisplay();
-  display.display();*/
-
   //disco lights setup
   strip.Begin();
+  setupAnimations();
   strip.Show();
-  SetupAnimations();
+
+  //oled
+  delay(150);
+  oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  oled.setRotation(2);
+  oled.drawBitmap(0, 0, fsLogo, 128, 32, SSD1306_WHITE);
+  oled.display();
+  delay(1000);
+  oled.clearDisplay();
+  oled.display();
 }
 
 void loop()
-{ /*
-  // text display tests
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);
-  display.print("Connecting to SSID\n'adafruit':");
-  display.print("connected!");
-  display.println("IP: 10.0.1.23");
-  display.print("Sending: ");
-  display.print(analogRead(A0));
-  display.setCursor(0, 0);
-  display.display(); // actually display all of the above
-  delay(200);*/
+{
+  //code to run every 15 ms
+  static uint32_t millis10ms = millis();
+  if ((millis() - millis10ms) >= 15)
+  {
+    static int16_t spriteOffset = -18;
+    millis10ms = millis();
+    oled.clearDisplay();
+    oled.drawBitmap(spriteOffset, 10 * sin((float)spriteOffset / 30.1), shakebot, 18, 32, SSD1306_WHITE);
+    oled.display();
+    spriteOffset++;
+    if (spriteOffset >= 128)
+    {
+      spriteOffset = -18;
+    }
+  }
 
-  //disco lights updates
-  animations.UpdateAnimations();
-  strip.Show();
+  //code to run every 60 ms
+  static uint32_t millis60ms = millis();
+  if ((millis() - millis60ms) >= 253)
+  {
+    millis60ms = millis();
+    updateAnimations();
+    strip.Show();
+  }
+  /*
+  // text display tests
+  oled.setTextSize(1);
+  oled.setTextColor(SSD1306_WHITE);
+  oled.setCursor(0, 0);
+  oled.print("Connecting to SSID\n'adafruit':");
+  oled.print("connected!");
+  oled.println("IP: 10.0.1.23");
+  oled.print("Sending: ");
+  oled.print(analogRead(A0));
+  oled.setCursor(0, 0);
+  oled.display(); // actually display all of the above
+  delay(200);*/
 }
